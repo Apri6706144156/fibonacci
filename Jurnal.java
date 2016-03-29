3349c616537685b7d14bfc584be0db443b213ad5
Apri Riyarahayu <ariyarahayu@gmail.com>
2016-03-29 09:34:33 +0700

    Added files via upload

---
+++ Jurnal.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jurnal;
+
+/**
+ *
+ * @author Praktikum
+ */
+import java.util.Scanner;
+public class Jurnal {
+
+	public static void main(String[] args) { 
+		Scanner input = new Scanner(System.in);
+		int num1=0,num2=1,jum;
+		int count;
+		
+		System.out.println("penjumlahan angka" +
+				" beberapa digit angka di input oleh user");
+		
+		System.out.println("enter N");
+		int N = input.nextInt();
+		System.out.println("pertama " + N  + " angka di fibonacci" +
+				"angka yang diikuti");
+		
+		
+		
+
+		for (count = 1;count<= N; count++) { 
+		jum=num1+num2; 
+		num1=num2; 
+		num2=jum; 
+		System.out.print(" "+num1); 
+	} 
+    } 
+} 
+        
